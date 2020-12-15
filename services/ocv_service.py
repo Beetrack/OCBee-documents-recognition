99f4dd0e423f0f539bfce1b5dc3755d111c8f634
Rodrigo Hanuch <rodrigo.hanuch@beetrack.com>
2020-12-15 17:14:27 -0300

    feat: step 3 ocv

--- services/ocv_service.py
+++ services/ocv_service.py
@@ -67,3 +67,70 @@ class OCVService:
         image_out = self._block_image_process(image_in, block_size, delta)
         image_out = self._postprocess(image_out)
         return image_out
+
+    def _sigmoid(self, x, orig, rad):
+        """Composing function to separate background"""
+        k = np.exp((x - orig) * 5 / rad)
+        return k / (k + 1.)
+
+    def _combine_block(self, img_in, mask):
+        """Combines all the local blocks into one image."""
+        # First, we pre-fill the masked region of img_out to white
+        # (i.e. background). The mask is retrieved from previous section.
+        img_out = np.zeros_like(img_in)
+        img_out[mask == 255] = 255
+        fimg_in = img_in.astype(np.float32)
+
+        # Then, we store the foreground (letters written with ink)
+        # in the `idx` array. If there are none (i.e. just background),
+        # we move on to the next block.
+        idx = np.where(mask == 0)
+        if idx[0].shape[0] == 0:
+            img_out[idx] = img_in[idx]
+            return img_out
+
+        # We find the intensity range of our pixels in this local part
+        # and clip the image block to that range, locally.
+        lo = fimg_in[idx].min()
+        hi = fimg_in[idx].max()
+        v = fimg_in[idx] - lo
+        r = hi - lo
+
+        # Now we use good old OTSU binarization to get a rough estimation
+        # of foreground and background regions.
+        img_in_idx = img_in[idx]
+        ret3, th3 = cv2.threshold(img_in[idx], 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+        # Then we normalize the stuffs and apply sigmoid to gradually
+        # combine the stuffs.
+        bound_value = np.min(img_in_idx[th3[:, 0] == 255])
+        bound_value = (bound_value - lo) / (r + 1e-5)
+        f = (v / (r + 1e-5))
+        f = self._sigmoid(f, bound_value + 0.05, 0.2)
+
+        # Finally, we re-normalize the result to the range [0..255]
+        img_out[idx] = (255. * f).astype(np.uint8)
+        return img_out
+
+    def _combine_block_image_process(self, image, mask, block_size):
+        """
+        Combination routine on local blocks, so that the scaling parameters
+        of Sigmoid function can be adjusted to local setting
+        """
+        out_image = np.zeros_like(image)
+        for row in range(0, image.shape[0], block_size):
+            for col in range(0, image.shape[1], block_size):
+                idx = (row, col)
+                block_idx = self._get_block_index(image.shape, idx, block_size)
+                out_image[block_idx] = self._combine_block(image[block_idx], mask[block_idx])
+        return out_image
+
+    def _combine_postprocess(self, image):
+        return image
+
+    def combine_process(self, img, mask):
+        """Executes whole pipeline and returns a mask for the original image. Returns cv2 image"""
+        image_in = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        image_out = self._combine_block_image_process(image_in, mask, 20)
+        image_out = self._combine_postprocess(image_out)
+        return image_out
