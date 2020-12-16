3840a2a1ae858c09f7f536498d9de936f2fdd70f
Rodrigo Hanuch <rodrigo.hanuch@beetrack.com>
2020-12-16 12:19:40 -0300

    test: add gamma function tests

--- tests/services/test_ocv_service.py
+++ tests/services/test_ocv_service.py
@@ -16,3 +16,22 @@ class OCVServiceTest(unittest.TestCase):
         del self.service
         del self.img_dir
         del self.img
+
+    def test_gamma_return_type(self):
+        # must return correctly with all of this cases
+        self.assertTrue(type(self.service.adjust_gamma(self.img)) is ndarray)
+        self.assertTrue(type(self.service.adjust_gamma(self.img, gamma=1)) is ndarray)
+        self.assertTrue(type(self.service.adjust_gamma(self.img, gamma=1.1)) is ndarray)
+
+    def test_gamma_args(self):
+        # img type must be ndarray
+        self.assertRaises(TypeError, self.service.adjust_gamma, '')
+
+        # gamma must be greater that zero
+        self.assertRaises(ValueError, self.service.adjust_gamma, self.img, gamma=0)
+        self.assertRaises(ValueError, self.service.adjust_gamma, self.img, gamma=-1)
+        self.assertRaises(ValueError, self.service.adjust_gamma, self.img, gamma=-2.1)
+
+        # gamma can be float or int and nothing else
+        self.assertRaises(TypeError, self.service.adjust_gamma, self.img, gamma='1')
+
