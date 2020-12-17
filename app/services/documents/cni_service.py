92339cbd81d51ffc88fbebaafc72a46b141b53a8
Rodrigo Hanuch <rodrigo.hanuch@beetrack.com>
2020-12-17 11:58:22 -0300

    docs: general documentation

--- app/services/documents/cni_service.py
+++ app/services/documents/cni_service.py
@@ -6,6 +6,24 @@ from base_document_service import BaseDocumentService
 
 
 class CNIService(BaseDocumentService):
+    """
+    CNI: Cedula Nacional de Identidad parsing service.
+
+    Patterns that are required (re format) (`PATTERNS`):
+        - RUN
+        - Nacionality and Sex
+        - Birthdate and Document number
+        - Generated date of document and Due date of document
+
+    Patterns that are normally obtained by OCV and are required to parse document correctly (`TO_FIND`):
+        - 'RUN'
+        - 'APELLIDOS'
+        - 'NOMBRES'
+        - 'NACIONALIDAD SEXO' (we apply also 'NACIONALIDAD' and 'SEXO' separately as OCV
+                               can sometimes interpret them as separte items)
+        - 'FECHA DE NACIMIENTO NUMERO DOCUMENTO'
+        - 'FECHA DE EMISION FECHA DE VENCIMIENTO'
+    """
 
     # patterns to make final clean
     PATTERNS = {
@@ -24,7 +42,7 @@ class CNIService(BaseDocumentService):
     # ...
     # NACIONALIDAD SEXO, CHILENO M,
     # ...
-    # FECHA DE NACIMIENTO NUMERO DOCUMENTO, 31 DIC 1997 511408104
+    # FECHA DE NACIMIENTO NUMERO DOCUMENTO, 31 DIC 1997 511.408.104
     # ...
     # FECHA DE EMISION FECHA DE VENCIMIENTO, 13 MAR 2017 31 DIC 2020
     TO_FIND = {
@@ -39,34 +57,62 @@ class CNIService(BaseDocumentService):
     }
 
     def _associate(self, text_list: list, threshold=0.75) -> dict:
+        """
+        Initial association of cleaned text into the corresponding dictionary; this is fields that are
+        searched for filled with the corresponding information
+        
+        Args:
+            text_list (list): cleaned list of strings that will be searched upon to associate
+                              to the desired document elements
+            threshold (optional)(int/float): threshold to use when validating the similarity of the search for key words
+
+        Returns:
+            dict: dictionary of generated associations of fields (i.e.: {field: information})
+        """
         association = {txt: None for txt in self.TO_FIND}
 
         for finding in self.TO_FIND.keys():
             for j, text in enumerate(text_list):
-                # RUN is read in different way
                 if self._valid_similarity(finding, text, threshold=threshold):
+                    # APELLIDOS are generally in two separate lines so we have to
+                    # concatenate them into a single string by looking into the next
+                    # element in the list
                     if finding == 'APELLIDOS':
                         association['APELLIDOS'] = text_list[j+1] + ' ' + text_list[j+2]
                     else:
                         association[finding] = text_list[j + self.TO_FIND[finding]]
-
+                # RUN is normally in the same string instead of a different one, so we
+                # have to apply regex to know if the text has the pattern of a RUN
                 elif re.match(self.PATTERNS['run'], text):
                     association['RUN'] = text
 
         return association
 
     def _valid_association(self, associations: dict) -> bool:
+        """
+        Validates that the initial association is correct and correctly fills all the required
+        fields of the document
+
+        Args:
+            associations (dict): dictionary of previously generated associations
+
+        Returns:
+            bool: True if all required fields are correctly fielled, False otherwise
+        """
         def valid_run(associations):
             run = associations['RUN']
             return bool(run) and re.match(self.PATTERNS['run'], run)
 
         def valid_lastname(associations):
+            # we cannot check with regex for names as they are unique in form and style
             return bool(associations['APELLIDOS'])
 
         def valid_name(associations):
+            # we cannot check with regex for names as they are unique in form and style
             return bool(associations['NOMBRES'])
 
         def valid_nationality_sex(associations):
+            # we have to validate a combination as they may be separated, but both have to be present
             return bool(associations['NACIONALIDAD SEXO'] or (associations['NACIONALIDAD'] and associations['SEXO']))
 
         def valid_birth_doc(associations):
@@ -87,6 +133,18 @@ class CNIService(BaseDocumentService):
         ))
 
     def _clean_processed_text(self, associations: dict) -> dict:
+        """
+        Cleans the processed text and splits and extracts the desired information from the association
+        to have the final information with the correct association
+
+        i.e.: before -> {'NAC SEX': 'CHILE M'} -> after {'NAC': 'CHILE', 'SEX': 'M'}
+
+        Args:
+            associations (dict): dictionary of previously generated and cleaned associations
+
+        Returns:
+            dict: correctly formatted associations and information
+        """
         def clean_nac_sex(associations):
             if 'NACIONALIDAD' in associations.keys():
                 nac, sex = re.match(self.PATTERNS['nac_sex'], associations['NACIONALIDAD']).groups()
@@ -125,4 +183,3 @@ class CNIService(BaseDocumentService):
         associations = clean_birth_doc(associations)
         associations = clean_generated_due(associations)
         return associations
-
