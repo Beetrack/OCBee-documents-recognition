4912879210b617836bc100afd6e7a285f38baa8b
Rodrigo Hanuch <rodrigo.hanuch@beetrack.com>
2020-12-18 12:36:21 -0300

    refactor: rename of main app

--- app/api/app.py
+++ app/api/app.py
@@ -1,34 +1,25 @@
+"""
+Main module to run Flask API
+"""
+# pylint: disable=import-error
 import os
-from pprint import pprint
-from flask import request, url_for
-from flask_api import FlaskAPI, status, exceptions
-from werkzeug.utils import secure_filename
 from uuid import uuid4
+from flask import request
+from flask_api import FlaskAPI, status
+from werkzeug.utils import secure_filename
+# pylint: enable=import-error
 
-# own services
-from app.services.ocv_service import OCVService
-from app.services.documents.cni_service import CNIService
-from app.services.documents.basic_service import BasicService
-
-
-UPLOAD_FOLDER = 'app/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-SERVICES = {
-    'ocv': OCVService(),
-    'basic': BasicService(),
-    'cni': CNIService()
-}
 
 app = FlaskAPI(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SERVICES'] = SERVICES
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    """Determines if the filetype is allowed or not accordingly to its name"""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 def save_file(file) -> str:
+    """Saves the uploaded file temporarily into 'app/uploads'"""
     # we generate a unique filename to prevent the case of two pictures
     # being named the same at the same time
     file_name = secure_filename(str(uuid4()))
@@ -39,19 +30,20 @@ def save_file(file) -> str:
 
 
 def app_service(name):
-    # simple method to compact call
+    """Method to compact call from other functions and return the corresponding association"""
     if name not in app.config['SERVICES'].keys():
         raise KeyError('Invalid service name.')
     return app.config['SERVICES'][name]
 
-
+# we disable redefinition of outer name as pylint thinks `request` is
+# being redefined but it is really not happening
+# pylint: disable=redefined-outer-name
 def process_image(request, service_name, threshold=0.75):
     file_path = save_file(request.files['file'])
 
     # concatenates result, passing directly what is read to the processing
     service = app_service(service_name)
-    result = service.process_text(app_service(
-        'ocv').process(file_path), threshold=threshold)
+    result = service.process_text(app.config['OCV'].process(file_path), threshold=threshold)
 
     # clean written image
     os.remove(file_path)
@@ -59,15 +51,16 @@ def process_image(request, service_name, threshold=0.75):
 
 
 def extract_threshold(request):
-    threshold = request.data['threshold'] if 'threshold' in request.data.keys(
-    ) else None
+    threshold = request.data['threshold'] if 'threshold' in request.data.keys() else None
     if threshold and threshold.isnumeric():
         return min(1.0, max(0.1, float(threshold)))
     return 0.75
-
+# pylint: enable=redefined-outer-name
 
 @app.route('/api/<string:service>', methods=['GET', 'POST'])
 def analyze_image(service):
+    # allow access through any string format
+    service = service.lower()
     try:
         # we extract (if existing) the threshold and apply a 'roof' to cap it at 1
         threshold = extract_threshold(request)
@@ -84,8 +77,5 @@ def analyze_image(service):
     except KeyError as error:
         result = ({'error': str(error)}, status.HTTP_400_BAD_REQUEST)
     finally:
-        return result
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        # disabling of lost exception as it is being handled
+        return result # pylint: disable=lost-exception
