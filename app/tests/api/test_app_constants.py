bfcfd79ada2c2e49fa188caf152cc1d9957822a6
Rodrigo Hanuch <rodrigo.hanuch@beetrack.com>
2020-12-21 13:53:07 -0300

    test: add constants

---
+++ app/tests/api/test_app_constants.py
@@ -0,0 +1,39 @@
+RUN_INTERPRETATION = [
+    'CEDULA DE',
+    'IDENTIDAD',
+    'BE CO',
+    'DSC I',
+    'RUN 5.632.605-7',
+    'REPUBLICA DE CHILE',
+    'SERVICIO DE REGIS',
+    'RO C',
+    'VIL F IDENTIFICAC ON',
+    'APELLIDOS',
+    'MALDONADO',
+    'JEREZ',
+    'NOMBRES',
+    'JUAN DANIEL',
+    'NACIONALIDAD SEXO',
+    'CHILENA M',
+    'FECHA DE NACIMIENTO NUMERO DOCUMENTO',
+    '15 MAR 1948 102.773.350',
+    'FECHA DE EMISION FECHA DE VENCIMIENTO',
+    '31 JUL 2014 15 MAR 2020',
+    'FIRMA DEL TITULAR',
+    'PIE',
+    '4 ON',
+    '5',
+    '2.505-7'
+]
+
+RUN_DICT = {
+    'run': '5.632.605-7',
+    'apellidos': 'MALDONADO JEREZ',
+    'nombres': 'JUAN DANIEL',
+    'nacionalidad': 'CHILENA',
+    'sexo': 'M',
+    'fecha_de_nacimiento': '15 MAR 1948',
+    'numero_documento': '102.773.350',
+    'fecha_de_emision': '31 JUL 2014',
+    'fecha_de_vencimiento': '15 MAR 2020'
+}
